0e7c8fd5ffcd731ec0cd22b633833a080d2cd8c0
firozspar <155627708+firozspar@users.noreply.github.com>
2024-08-30 01:14:17 +0530

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -4,7 +4,7 @@ const msalConfig = {
     auth: {
         clientId: "c5755403-ca31-432f-b60e-bcf0fc64f7db", // Replaced with our client ID
         authority: "https://login.microsoftonline.com/48342a33-25ad-4d79-b854-4d66877e41c1", // Replaced with our tenant ID
-        redirectUri: "https://victorious-glacier-0f67efe10.5.azurestaticapps.net", // Replaced with our redirect URI
+        redirectUri: "https://blue-desert-0e4d91a10.5.azurestaticapps.net", // Replaced with our redirect URI
     },
     cache: {
         cacheLocation: "localStorage",
