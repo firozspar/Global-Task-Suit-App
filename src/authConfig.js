7f31f5ee72c0485f6706d4dff9971a2e2218b7ef
Firoz Ahmad <firoz.ahmad@sparsolutions.com>
2024-08-29 14:22:52 +0530

    redirect URI has been changed

--- src/authConfig.js
+++ src/authConfig.js
@@ -4,7 +4,7 @@ const msalConfig = {
     auth: {
         clientId: "c5755403-ca31-432f-b60e-bcf0fc64f7db", // Replaced with our client ID
         authority: "https://login.microsoftonline.com/48342a33-25ad-4d79-b854-4d66877e41c1", // Replaced with our tenant ID
-        redirectUri: "http://localhost:3000", // Replaced with our redirect URI
+        redirectUri: "https://victorious-glacier-0f67efe10.5.azurestaticapps.net", // Replaced with our redirect URI
     },
     cache: {
         cacheLocation: "localStorage",
