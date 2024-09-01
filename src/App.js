fe0a03c282fb08827e3d0f109dfdd0fe46274a18
Firoz Ahmad <firoz.ahmad@sparsolutions.com>
2024-09-02 01:59:48 +0530

    added configuration file

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { ThemeProvider, createTheme } from '@mui/material/styles';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import Login from './pages/Login';
+// import Dashboard from './pages/Dashboard';
+// import Createtask from './pages/CreateTask';
+
+// const theme = createTheme({
+//   // will customize the theme here later
+// });
+
+
+// const App = () => {
+//   return (
+//     <ThemeProvider theme={theme}>
+//     <CssBaseline />
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Login />} />
+//         <Route path="/dashboard" element={<Dashboard />} />
+//         <Route path="/createtask" element={<Createtask />} />
+//       </Routes>
+//     </Router>
+//   </ThemeProvider>
+    
+//   );
+// };
+
+// export default App;
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import Createtask from './pages/CreateTask';
+import CreateTask from './pages/CreateTask';
 
-const theme = createTheme({
-  // will customize the theme here later
-});
-
-
-const App = () => {
+function App() {
   return (
-    <ThemeProvider theme={theme}>
-    <CssBaseline />
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/createtask" element={<Createtask />} />
+        <Route path="/createtask" element={<CreateTask />} />
+        {/* Replace Redirect with Navigate */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
-  </ThemeProvider>
-    
   );
-};
+}
 
 export default App;
+
 
--- src/authConfig.js
+++ src/authConfig.js
@@ -1,10 +1,11 @@
 import { PublicClientApplication } from "@azure/msal-browser";
 
+const isLocalhost = window.location.hostname === "localhost";
 const msalConfig = {
     auth: {
         clientId: "201f9e01-322a-402b-866d-00b52b65c91a", // Replaced with our client ID
         authority: "https://login.microsoftonline.com/48342a33-25ad-4d79-b854-4d66877e41c1", // Replaced with our tenant ID
-        redirectUri: "https://blue-desert-0e4d91a10.5.azurestaticapps.net", // Replaced with our redirect URI
+        redirectUri: isLocalhost ? "http://localhost:3000" : "https://blue-desert-0e4d91a10.5.azurestaticapps.net" // Replaced with our redirect URI
     },
     cache: {
         cacheLocation: "localStorage",
